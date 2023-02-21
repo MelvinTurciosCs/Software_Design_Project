a3d19cc571487fe7cd6b163bd7a14d562fe398af
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2023-02-21 10:44:30 -0600

    Added the form to Login.php

---
+++ Login.php
@@ -0,0 +1,11 @@
+<?php 
+  include('includes/header.php');
+?>
+
+<!-- This is the code for the carousel images in the background -->
+
+<!-- This is the form to create a login page -->
+<form action="login.php" method="post">
+  <input type="text" name="username" placeholder="Username">
+  <input type="password" name="password" placeholder="Password">
+  <input type="submit" name="submit" value="Login">
