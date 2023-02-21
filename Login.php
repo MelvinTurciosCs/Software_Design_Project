ed3c8e4b086e14ad19ba149890d8ee62f53246b2
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2023-02-21 10:46:46 -0600

    Added the carousel images to Login.php

--- Login.php
+++ Login.php
@@ -3,6 +3,27 @@
 ?>
 
 <!-- This is the code for the carousel images in the background -->
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/OIP.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/Georgia Natural gas.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/Total.jpeg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 <!-- This is the form to create a login page -->
 <form action="login.php" method="post">
