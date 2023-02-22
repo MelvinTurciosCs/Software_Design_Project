1da0fb59a629080cfcc4bf836c6ea84d844cf198
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-02-22 16:35:20 -0600

    modifications

--- fuelhistory.php
+++ fuelhistory.php
@@ -13,11 +13,32 @@
       <th>Total Amount Due</th>
     </tr>
     <tr>
-      <td input type="int" name="gallons_requested" required>500</td>
+      <td input type="int" name="gallons_requested" required>5000</td>
+      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
+      <td input type="date" name="delivery_date" required>02/20/2023</td>
+      <td input type="int" name="suggested_price" required>$6.00</td>
+      <td input type="int" name="total_price" required>$20200.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>6000</td>
       <td input type="text" name="delivery_address" required>2245 Avenue Road Drive</td>
-      <td input type="date" name="delivery_date" required>02/23/2023</td>
-      <td input type="int" name="suggested_price" required>$50.00</td>
-      <td input type="int" name="total_price" required>$20000.00</td>
+      <td input type="date" name="delivery_date" required>02/22/2023</td>
+      <td input type="int" name="suggested_price" required>$6.00</td>
+      <td input type="int" name="total_price" required>$24000.23</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>6500</td>
+      <td input type="text" name="delivery_address" required>2245 George Town Rd</td>
+      <td input type="date" name="delivery_date" required>02/27/2023</td>
+      <td input type="int" name="suggested_price" required>$5.45</td>
+      <td input type="int" name="total_price" required>$27050.66</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
     </tr>
   </table>
 </div>
