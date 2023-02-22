1da70771dc2b8a5228500570c0f8d200053fadc4
EricIHernandez <ericih97@gmail.com>
2023-02-22 17:10:26 -0600

    Removed commented dummy code

--- Register.php
+++ Register.php
@@ -45,28 +45,3 @@
 
 </html>
 
-<!--<section class="my-4">
-    <div class="py-4">
-      <h2 class="text-center">Contact Us</h2>
-    </div>
-
-    <div class="w-50 m-auto">
-      <form action="about.php" method="post">
-          <div class="form-group">
-              <label>Name:</label>
-              <input type="text" name="name" class="form-control">
-          </div>
-          <div class="form-group">
-              <label>Email:</label>
-              <input type="email" name="email" class="form-control">
-          </div>
-          <div class="form-group">
-              <label>Number:</label>
-              <input type="text" name="number" class="form-control">
-          </div>
-          <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-    </div>
-
-</section>
--->
