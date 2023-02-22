ee3a77fd13d27d0a385a5aeacca75870aecf609c
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-02-21 18:32:43 -0600

    history page

---
+++ fuelhistory.php
@@ -0,0 +1,26 @@
+<?php 
+  include('includes/header.php');
+?>
+
+<div>
+  <h1>Fuel Quote History</h1>
+  <table border = "1">
+    <tr>
+      <th>Location</th>
+      <th>Date</th>
+      <th>Time</th>
+      <th>Price</th>
+    </tr>
+    <tr>
+      <td>Shell Gas Corp TX1032419</td>
+      <td>2/23/2023</td>
+      <td>22:11</td>
+      <td>$1312.21</td>
+    </tr>
+  </table>
+</div>
+
+
+<?php 
+  include('includes/footer.php');
+?>
