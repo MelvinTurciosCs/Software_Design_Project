6a047e137f975da1e4036fe8fc59ad8b2a1f0178
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2023-02-22 17:49:04 -0600

    Changed the registration page to look like the login page

--- Register.php
+++ Register.php
@@ -8,40 +8,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-
-
 <body>
-    <div class="py-4">
-      <h2 class="text-center">Contact Us</h2>
-    </div>
+    <!-- Resister Form -->
+    <form action="">
+        <div class="headingContainer">
+            <h3>Sign Up</h3>
+            <p>Create an account</p>
+        </div>
 
-    <div class="w-50 m-auto">
-      <form action="about.php" method="post">
-          <div class="form-group">
-              <label>Username:</label>
-              <input type="text" name="username" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label>Email:</label>
-              <input type="text" name="username" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label>Password:</label>
-              <input type="password" name="password" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label>Confirm Password:</label>
-              <input type="password" name="password" class="form-control" required>
-          </div>
-          <button type="submit" class="btn btn-success">Register</button>
-          <button type="submit" class="btn btn-success">Already have an account?</button>
-      </form>
+        <!-- Main container for all inputs -->
+        <div class="mainContainer">
+
+        <!-- Username input -->
+        <label for="username">Username: </label>
+        <input type="text" name="username" id="username" placeholder="Enter Username" required>
+
+        <!-- Username input -->
+        <label for="email">Email: </label>
+        <input type="text" name="username" id="email" placeholder="Enter Email" required>
+
+        <!-- Password input -->
+        <label class="loginPassword" for="password">Password: </label>
+        <input type="password" name="password" id="password" placeholder="Enter Password" required>
+
+        <!-- Password Confirmation input -->
+        <label class="loginPassword" for="confirm_password">Confirm Password: </label>
+        <input type="password" name="password" id="password" placeholder="Confirm Password" required>
+
+        <!-- Create Account button -->
+        <button class="submit_Button " type="submit">Create Account</button> 
+
+     
+        <!-- Sign up link -->
+        <p class="register">Already have an account? <a href="#">Login</a></p>
+    </div>
+ </form>
+    
 
     </div>
 </body>
-
-
 </html>
 
