e6be1ac74f320dd0276e5403e646b82a4ab87b8a
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-02-22 16:52:28 -0600

    final

--- fuelhistory.php
+++ fuelhistory.php
@@ -2,9 +2,9 @@
   include('includes/header.php');
 ?>
 
-<div>
-  <h1>Fuel Up Quote History</h1>
-  <table border = "1">
+  <h1 style = "text-align: center;" >Fuel Up Quote History</h1>
+  <div style="text-align: center;">
+  <table border = 1 style="margin: 0 auto;">
     <tr>
       <th>Gallons Requested</th>
       <th>Delivery Address</th>
@@ -32,6 +32,69 @@
       <td input type="date" name="delivery_date" required>02/27/2023</td>
       <td input type="int" name="suggested_price" required>$5.45</td>
       <td input type="int" name="total_price" required>$27050.66</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>5000</td>
+      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
+      <td input type="date" name="delivery_date" required>02/20/2023</td>
+      <td input type="int" name="suggested_price" required>$6.00</td>
+      <td input type="int" name="total_price" required>$20200.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>300</td>
+      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
+      <td input type="date" name="delivery_date" required>02/25/2023</td>
+      <td input type="int" name="suggested_price" required>$4.54</td>
+      <td input type="int" name="total_price" required>$1000.00</td>
+    </tr>
+    <tr>
+      <td input type="int" name="gallons_requested" required>5000</td>
+      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
+      <td input type="date" name="delivery_date" required>02/20/2023</td>
+      <td input type="int" name="suggested_price" required>$6.00</td>
+      <td input type="int" name="total_price" required>$20200.00</td>
     </tr>
     <tr>
       <td input type="int" name="gallons_requested" required>300</td>
