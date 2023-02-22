11d4026ab307ca9e593d0288958279a8ff829cdc
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-02-22 15:52:55 -0600

    added restrictions to table

--- fuelhistory.php
+++ fuelhistory.php
@@ -3,19 +3,21 @@
 ?>
 
 <div>
-  <h1>Fuel Quote History</h1>
+  <h1>Fuel Up Quote History</h1>
   <table border = "1">
     <tr>
-      <th>Location</th>
-      <th>Date</th>
-      <th>Time</th>
-      <th>Price</th>
+      <th>Gallons Requested</th>
+      <th>Delivery Address</th>
+      <th>Delivery Date</th>
+      <th>Suggested Price / Gallon</th>
+      <th>Total Amount Due</th>
     </tr>
     <tr>
-      <td>Shell Gas Corp TX1032419</td>
-      <td>2/23/2023</td>
-      <td>22:11</td>
-      <td>$1312.21</td>
+      <td input type="int" name="gallons_requested" required>500</td>
+      <td input type="text" name="delivery_address" required>2245 Avenue Road Drive</td>
+      <td input type="date" name="delivery_date" required>02/23/2023</td>
+      <td input type="int" name="suggested_price" required>$50.00</td>
+      <td input type="int" name="total_price" required>$20000.00</td>
     </tr>
   </table>
 </div>
