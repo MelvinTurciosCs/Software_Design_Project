6925dc8afc86842dca814fdb9ada03408e995771
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-03-19 16:55:54 -0500

    fuel req

--- Fuel_Req_Form.php
+++ Fuel_Req_Form.php
@@ -2,53 +2,60 @@
 include('includes/header.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=h, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>Fuel_quote_Form</title>
-  </head>
-  <body>
+<div class="request_form">
 
-    <div class="fuel_QForm">
-      <h1>Fuel Quote Form</h1>
-    <form action="Proposed_quotes.html" method="GET"> 
-      
-        <!--used to type the gallons requesteds-->
-        <label for = "Gal">Gallons Requested:</label>
-        <input class ="GallReq" name = "GalReq" type="number" id="Gal" placeholder="Number of Gallons" min="1" max="1000"/>
-      
-      
-        <!--Used to type address-->
-        <label for = "ADDRESS">Address:</label>
-        <input class = "Addy" name = "Address" type="text" id="ADDRESS" placeholder="Address"/>
-      
-      
-        <!--used for delivery date-->
-        <label for="DELVDATE">Delivery Date:</label>
-        <input class="DelDate" name = "DELIVDate" type="Date"  id="DELVDATE" placeholder="Delivery Date"/>
-      
-      
-        <!--should display the suggested price-->
-        <div>
-          <label>Suggested Price per Gallon:</label>
-        </div>
-        
-        <!--Should display total price -->
-        <div>
-          <label>Total Price</label> 
-        </div>
-        
-      
-      <button class="SUB_Button">Submit</button>
-    </form>
+  <div class="container">
+    <h1>Customer info</h1>
+    <div>
+    <b>Name: Melvin T<b>
+    </div>
+    <div>
+      <b>Address 1: 123 apple Dr</b>
+    </div>
+    <div>
+      <b>City: Houston</b>
+    </div>
+    <div>
+      <b>State: TX</b>
+    </div>
+    <div>
+      <b>Zipcode: 66666</b>
+    </div>
+   
+
   </div>
 
-  </body>
-</html>
+  <div class="container">
+    <h1>Fuel Quote Form</h1>
+    <!--used to type the gallons requesteds-->
+    <label for="Gal">Gallons Requested:</label>
+    <input class="GallReq" name="GalReq" type="number" id="Gal" placeholder="Number of Gallons" min="1" max="1000" />
+
+    <!--Used to type address-->
+    <label for="ADDRESS">Address:</label>
+    <input class="Addy" name="Address" type="text" id="ADDRESS" placeholder="Address" />
+
+
+    <!--used for delivery date-->
+    <label for="DELVDATE">Delivery Date:</label>
+    <input class="DelDate" name="DELIVDate" type="Date" id="DELVDATE" placeholder="Delivery Date" />
+
+
+    <!--should display the suggested price-->
+    <div>
+      <label>Suggested Price per Gallon:</label>
+    </div>
+
+    <!--Should display total price -->
+    <div>
+      <label>Total Price</label>
+    </div>
+
+
+    <button class="SUB_Button">Submit</button>
+  </div>
+
+</div>
 
 <?php
 include('includes/footer.php');
