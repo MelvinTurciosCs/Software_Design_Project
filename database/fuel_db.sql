1232649b35851ff7bc4fecc7cf3507d31574c28c
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-03-20 16:45:03 -0500

    gg

--- database/fuel_db.sql
+++ database/fuel_db.sql
@@ -60,7 +60,7 @@ INSERT INTO `client` (`client_ID`, `username`, `password`, `name`, `address_1`, 
 CREATE TABLE `order_history` (
   `order_ID` int(11) NOT NULL,
   `total_price` float DEFAULT NULL,
-  `client_id` varchar(45) NOT NULL, 
+  `client_id` varchar(11) NOT NULL, 
   `delv_date` date DEFAULT NULL,
   `delv_adress` date DEFAULT NULL,
   `ccpm` float DEFAULT NULL,
