3c6a082be9dd684eb4b7e1c48dd3a38a7b268ff6
guillermo5431 <92940444+guillermo5431@users.noreply.github.com>
2023-03-20 19:27:09 -0500

    updated fuel_db

--- database/fuel_db.sql
+++ database/fuel_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2023 at 11:11 PM
+-- Generation Time: Mar 21, 2023 at 01:26 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -38,11 +38,12 @@ CREATE TABLE `client` (
   `state` varchar(2) DEFAULT NULL,
   `zipcode` int(9) DEFAULT NULL,
   `email` varchar(25) DEFAULT NULL,
-  `cpm` float DEFAULT NULL,
-
-  PRIMARY KEY (client_ID)
+  `cpm` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `client`
+--
 
 INSERT INTO `client` (`client_ID`, `username`, `password`, `name`, `address_1`, `address_2`, `city`, `state`, `zipcode`, `email`, `cpm`) VALUES
 (0, 'alice123', '11111111', 'alice volto', '123 apple drive', '124 pineapple drive', 'Houston', 'TX', 77000, 'alice54@yahoo.com', 10),
@@ -57,24 +58,65 @@ INSERT INTO `client` (`client_ID`, `username`, `password`, `name`, `address_1`, 
 CREATE TABLE `order_history` (
   `order_ID` int(11) NOT NULL,
   `total_price` float DEFAULT NULL,
-  `client_id` int(11) NOT NULL, 
+  `user_ID` int(11) NOT NULL,
   `delv_date` date DEFAULT NULL,
-  `delv_adress` date DEFAULT NULL,
   `ccpm` float DEFAULT NULL,
   `request_Gals` int(10) DEFAULT NULL,
-  
-  PRIMARY KEY (order_ID),
-  FOREIGN KEY (client_id) REFERENCES client(client_ID)
+  `suggested_Price` float NOT NULL,
+  `del_Address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `order_history` (`order_ID`, `total_price`, `client_id`, `delv_date`, `delv_adress`, `ccpm`, `request_Gals`) VALUES
-(1, 20022, '0', '2023-04-12', '123 Houston Street Road', 40, 100);
+--
+-- Dumping data for table `order_history`
+--
 
+INSERT INTO `order_history` (`order_ID`, `total_price`, `user_ID`, `delv_date`, `ccpm`, `request_Gals`, `suggested_Price`, `del_Address`) VALUES
+(1, 20022, 8, '2023-04-12', 40, 100, 20, '123 apple st');
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`client_ID`);
+
+--
+-- Indexes for table `order_history`
+--
+ALTER TABLE `order_history`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `user_ID` (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
 ALTER TABLE `client`
   MODIFY `client_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT for table `order_history`
+--
 ALTER TABLE `order_history`
   MODIFY `order_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_history`
+--
+ALTER TABLE `order_history`
+  ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `client` (`client_ID`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
