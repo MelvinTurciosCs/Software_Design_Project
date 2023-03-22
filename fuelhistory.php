3a77d0ce871b6a2a1bef815fc3589f3758773d86
Juan Yanez <65358942+juanzenay@users.noreply.github.com>
2023-03-21 19:34:26 -0500

    Add files via upload

--- fuelhistory.php
+++ fuelhistory.php
@@ -1,114 +1,58 @@
-<?php 
-  include('includes/header.php');
+<?php
+include('includes/header.php');
 ?>
 
-  <h1 style = "text-align: center;" >Fuel Up Quote History</h1>
-  <div style="text-align: center;">
-  <table border = 1 style="margin: 0 auto;">
-    <tr>
-      <th>Gallons Requested</th>
-      <th>Delivery Address</th>
-      <th>Delivery Date</th>
-      <th>Suggested Price / Gallon</th>
-      <th>Total Amount Due</th>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>5000</td>
-      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
-      <td input type="date" name="delivery_date" required>02/20/2023</td>
-      <td input type="int" name="suggested_price" required>$6.00</td>
-      <td input type="int" name="total_price" required>$20200.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>6000</td>
-      <td input type="text" name="delivery_address" required>2245 Avenue Road Drive</td>
-      <td input type="date" name="delivery_date" required>02/22/2023</td>
-      <td input type="int" name="suggested_price" required>$6.00</td>
-      <td input type="int" name="total_price" required>$24000.23</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>6500</td>
-      <td input type="text" name="delivery_address" required>2245 George Town Rd</td>
-      <td input type="date" name="delivery_date" required>02/27/2023</td>
-      <td input type="int" name="suggested_price" required>$5.45</td>
-      <td input type="int" name="total_price" required>$27050.66</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>5000</td>
-      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
-      <td input type="date" name="delivery_date" required>02/20/2023</td>
-      <td input type="int" name="suggested_price" required>$6.00</td>
-      <td input type="int" name="total_price" required>$20200.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>5000</td>
-      <td input type="text" name="delivery_address" required>2245 Road Roundabout Av</td>
-      <td input type="date" name="delivery_date" required>02/20/2023</td>
-      <td input type="int" name="suggested_price" required>$6.00</td>
-      <td input type="int" name="total_price" required>$20200.00</td>
-    </tr>
-    <tr>
-      <td input type="int" name="gallons_requested" required>300</td>
-      <td input type="text" name="delivery_address" required>2245 Texas City New Bnd</td>
-      <td input type="date" name="delivery_date" required>02/25/2023</td>
-      <td input type="int" name="suggested_price" required>$4.54</td>
-      <td input type="int" name="total_price" required>$1000.00</td>
-    </tr>
-  </table>
-</div>
 
 
+<html>
+
+<head>
+    <title>Order History</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Gallons Requested</th>
+            <th>Delivery Address</th>
+            <th>Delivery Date</th>
+            <th>Suggested Price</th>
+            <th>Total Price</th>
+        </tr>
+        <?php
+
+        //access the dbconnection and functions php
+        require_once './includes/config/dbconnection.php';
+
+        //declare the session variable to fetch specific data, place on where user_id = #user_id
+        $user_id = $_SESSION["useruid"];
 
 
-<?php 
-  include('includes/footer.php');
+        //delete stuff below
+        $sql = "SELECT request_Gals, del_Address, delv_date, suggested_Price, total_price FROM order_history WHERE user_ID = '$user_id'";
+        $result = mysqli_query($con, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr><td>" . $row["request_Gals"] . "</td><td>" . $row["del_Address"] . "</td><td>" . $row["delv_date"] . "</td><td>" . $row["suggested_Price"] . "</td><td>" . $row["total_price"] . "</td></tr>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+        mysqli_close($con);
+        ?>
+    </table>
+</body>
+
+</html>
+
+
+<?php
+include('includes/footer.php');
+
+//$_session["useruid"]
+//$sql = "SELECT 'request_Gals', 'delv_adress', 'delv_date', 'ccpm', 'total_price'  FROM order_history WHERE "
+//`order_ID`, `total_price`, `client_name`, `delv_date`, `ccpm`, `request_Gals`
 ?>
