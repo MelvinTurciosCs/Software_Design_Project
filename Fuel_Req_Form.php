a1e5666eb9b5d966f1e8392474f50265411c6bd5
EricIHernandez <ericih97@gmail.com>
2023-03-21 22:25:27 -0500

    Updated Fuel_Req_Form, + Functions, +new pages

--- Fuel_Req_Form.php
+++ Fuel_Req_Form.php
@@ -1,13 +1,19 @@
 <?php
 include('includes/header.php');
 ?>
-
 <div class="request_form">
+  <?php
+    //access the dbconnection and functions php
+    require_once './includes/config/dbconnection.php';
+    require_once './includes/config/functions.php';
 
+    //declare the session variable to fetch specific data, place on where user_id = #user_id
+    $user_id = $_SESSION["useruid"];
+  ?>
   <div class="container">
     <h1>Customer info</h1>
     <div>
-    <b>Name: Melvin T<b>
+    <b>Name: <?php echo $user_id?><b>
     </div>
     <div>
       <b>Address 1: 123 apple Dr</b>
@@ -21,38 +27,25 @@ include('includes/header.php');
     <div>
       <b>Zipcode: 66666</b>
     </div>
-   
-
   </div>
+  <form action="/Fuel_Req_Form.php" method="post">
+    <div class="container">
+      <h1>Fuel Quote Form</h1>
+      <!--used to type the gallons requesteds-->
+      <label for="Gal">Gallons Requested:</label>
+      <input class="GallReq" name="GalReq" type="number" id="Gal" placeholder="Number of Gallons" min="1" max="1000" />
 
-  <div class="container">
-    <h1>Fuel Quote Form</h1>
-    <!--used to type the gallons requesteds-->
-    <label for="Gal">Gallons Requested:</label>
-    <input class="GallReq" name="GalReq" type="number" id="Gal" placeholder="Number of Gallons" min="1" max="1000" />
-
-    <!--Used to type address-->
-    <label for="ADDRESS">Address:</label>
-    <input class="Addy" name="Address" type="text" id="ADDRESS" placeholder="Address" />
-
-
-    <!--used for delivery date-->
-    <label for="DELVDATE">Delivery Date:</label>
-    <input class="DelDate" name="DELIVDate" type="Date" id="DELVDATE" placeholder="Delivery Date" />
-
+      <!--used for delivery date-->
+      <label for="DELVDATE">Delivery Date:</label>
+      <input class="DelDate" name="DELIVDate" type="Date" id="DELVDATE" placeholder="Delivery Date" />
+    </form>
 
     <!--should display the suggested price-->
     <div>
       <label>Suggested Price per Gallon:</label>
     </div>
 
-    <!--Should display total price -->
-    <div>
-      <label>Total Price</label>
-    </div>
-
-
-    <button class="SUB_Button">Submit</button>
+    <button class="SUB_Button" name = "submit">Submit</button>
   </div>
 
 </div>
---
+++ includes/config/Fuel_Req.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    //access the dbconnection and functions php
+    require_once 'dbconnection.php';
+    require_once 'functions.php';
+
+    //saves inputs into local vars
+    $gallons = $_POST["GalReq"];
+    
+    //function to check if user left empty fields
+    if(emptyInputGallons($gallons) !== false){
+        header("location: ../../Fuel_Req_Form.php?error=emptyinput");
+        exit();
+    }
+
+    $proposedgallons = fuelCalc($con, $gallons);
+}
+else 
+{
+    header("location: ../../Fuel_Req_Form.php");
+    exit();
+}
+
+
+?>
