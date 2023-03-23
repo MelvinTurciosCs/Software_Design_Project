93fd7f27303f0a663f70a14b43b82e9c7d48d39b
EricIHernandez <ericih97@gmail.com>
2023-03-22 20:51:08 -0500

    +unittest randomizer

--- unit_Test/test.php
+++ unit_Test/test.php
@@ -57,4 +57,28 @@ class AccountCreationTest extends TestCase
     }
 } 
 
+class FuelCalculationTest extends TestCase
+{
+    protected static $gallons;
+    protected static $userloc;
+    protected static $userhistory;
+    protected static $usercpm;
+
+    public static function setUpBeforeClass(): void
+    {
+        // Generate a unique username and password for testing
+        //gallons from 0 to 1,000,000
+        self::$gallons = rand(0, 100000);
+        //userloc from 0 to 1 (0 = in state, 1 = out of state)
+        self::$userloc = rand(0, 1);
+        //userhistory from 0 to 1 (0 = no history, 1 = history)
+        self::$userhistory = rand(0, 1);
+        //usercpm from 0 to 1, .1 increments 
+        self::$usercpm = rand(0, 10)/10;
+    }
+    
+
+}
+
+
 ?>
