63ddf3298b33ed65886f5240cde41ce9fa90903a
EricIHernandez <36204758+EricIHernandez@users.noreply.github.com>
2023-03-29 18:10:26 -0500

    Create Fuel_Req_Form.inc.php

---
+++ includes/config/Fuel_Req_Form.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    //access the dbconnection and functions php
+    require_once 'dbconnection.php';
+    require_once 'functions.php';
+
+    //saves inputs into local vars
+    $gallons = $_POST["GalReq"];
+
+    //function to check if user left empty fields
+    if(emptyInputGallons($gallons) !== false){
+        header("location: ../../Fuel_Req_Form.php?error=emptyinput");
+        exit();
+    }
+
+    $proposedgallons = fuelCalc($con, $gallons);
+    echo $proposedgallons;
+    exit();
+}
+else 
+{
+    header("location: ../../Fuel_Req_Form.php");
+    exit();
+}
+
+
+?>
