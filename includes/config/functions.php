ed8e7b2070bb8ffb5351d3c797547e500d498303
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-04-02 18:57:13 -0500

    functions fix

--- includes/config/functions.php
+++ includes/config/functions.php
@@ -168,7 +168,7 @@ else if($checkPwd === true){
 // Create the function for code for updating the user profile information
 function update_Profile_Info($con, $name, $address_1, $address_2, $city, $state, $zipcode, $cpm, $user_id){
    //the question mark prevents injections CHANGED
-    $sql = "UPDATE client SET name = ?, address_1 = ?, address_2 = ?, city = ?, state = ?, zipcode = ?, cpm = ? WHERE client_ID = ?; ";
+    $sql = "UPDATE client SET Name = ?, Address_1 = ?, Address_2 = ?, city = ?, state = ?, zipcode = ?, cpm = ? WHERE client_ID = ?; ";
 
     //prepared statement
     $stmt = mysqli_stmt_init($con);
