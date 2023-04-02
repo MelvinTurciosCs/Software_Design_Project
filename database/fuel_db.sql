e46429dcfda09034e3da00f6001ee01830635bcf
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-04-02 18:26:39 -0500

    fixes

--- database/fuel_db.sql
+++ database/fuel_db.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE login (
   client_ID int(11) NOT NULL,
   username varchar(16) DEFAULT NULL,
-  password varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
