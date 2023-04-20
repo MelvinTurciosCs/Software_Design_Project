9304ca3cc8d2eb8b13cce9726838d98eb102b4ed
juanzenay <65358942+juanzenay@users.noreply.github.com>
2023-04-20 14:01:38 -0500

    gg

--- Register.php
+++ Register.php
@@ -15,12 +15,12 @@ include('includes/header.php');
   <div class="mainContainer">
 
     <!-- Username input -->
-    <label for="username">Username: </label>
+    <label for="username">Username (Letters and numbers): </label>
     <input type="text" name="username" id="username" placeholder="Enter Username" required pattern="[a-zA-Z0-9]+"
     title="Username must contain only letters and numbers">
 
     <!-- password input -->
-    <label>Password: </label>
+    <label>Password (8 characters): </label>
     <input type="password" name="pwd" placeholder="Enter Password" required pattern=".{8,}"
     title="Password must be at least 8 characters long">
 
@@ -28,7 +28,7 @@ include('includes/header.php');
     <label class="loginPassword" for="confirm_password">Confirm Password: </label>
     <input type="password" name="pwdrepeat" id="password" placeholder="Confirm Password" required pattern=".{8,}"
     title="Password must be at least 8 characters long">
-    
+
     <!-- Create Account button -->
     <button class="submit_Button " type="submit" name="submit">Create Account</button>
 
